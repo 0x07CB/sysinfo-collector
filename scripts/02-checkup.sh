46629f2654df387992539025ef885d0f9dd73357
0x07cb <83157348+0x07CB@users.noreply.github.com>
2025-06-28 17:07:26 +0200

    Ajout de la vérification des mises à jour disponibles dans 02-checkup.sh et création du script 00-init.sh pour le diagnostic système. Mise à jour du fichier .gitignore pour inclure checkup.log.

--- scripts/02-checkup.sh
+++ scripts/02-checkup.sh
@@ -3,4 +3,24 @@
 # Checkup script for system diagnostics
 # ######################################
 
+# #### ETAPE [2]: Vérification des mises à jour disponibles ####
+# Description:
+#
+# "Un système obsolète est une faille en puissance."
+# — Détecte les paquets obsolètes. Mesure de prévention contre les vulnérabilités connues.
+#
+{
 
+    echo "[2] Vérification des mises à jour disponibles :"
+    if command -v apt >/dev/null; then
+        sudo apt update && apt list --upgradable
+    elif command -v dnf >/dev/null; then
+        sudo dnf check-update
+    elif command -v yum >/dev/null; then
+        sudo yum check-update
+    else
+        echo "Gestionnaire de paquets non reconnu."
+    fi
+
+
+} >> "$LOGFILE"
